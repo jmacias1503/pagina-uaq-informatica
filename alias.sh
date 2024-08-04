30ed988109ef6e04f93fd8205fd1f5cf00ea0e09
Alejandro <alex.mac.fon@gmail.com>
2024-08-04 00:47:31 -0600

    fixed: cap typo

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
-alias deploy="hugo server -D -F --NoHTTPCache"
+alias deploy="hugo server -D -F --noHTTPCache"
 alias news="hugo new content noticias/$(date '+noticia-%Y-%m-%d').md && $EDITOR content/noticias/$(date '+noticia-%Y-%m-%d').md"
