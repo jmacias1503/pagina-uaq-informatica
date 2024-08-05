74f517dff9fc989481cf50c2ecf1fa6f15ab347a
Alejandro <alex.mac.fon@gmail.com>
2024-08-04 19:03:50 -0600

    added: fast render disabled for alias

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
-alias hugo-deploy="hugo server -D -F --noHTTPCache"
+alias hugo-deploy="hugo server -D -F --noHTTPCache --disableFastRender"
 alias hugo-news="hugo new content noticias/$(date '+noticia-%Y-%m-%d').md && $EDITOR content/noticias/$(date '+noticia-%Y-%m-%d').md"
